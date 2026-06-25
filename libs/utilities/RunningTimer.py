18536b9fbdfea561f6b2fd92dc52ca7f65cb5469
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-06-25 15:13:19 +0900

    Update RunningTimer.py

--- libs/utilities/RunningTimer.py
+++ libs/utilities/RunningTimer.py
@@ -5,15 +5,31 @@ class RunningTimer:
         self.start_time = time.time()
         self.end_time = 0
 
-    def finish(self):
+        self.elapsed_total_seconds = 0
+        self.elapsed_hours = 0
+        self.elapsed_minutes = 0
+        self.elapsed_seconds = 0
+
+    def finish(self) -> "RunningTimer":
         self.end_time = time.time()
+        return self
 
-    def start(self):
+    def start(self) -> "RunningTimer":
         self.start_time = time.time()
+        return self
 
-    def result(self):
-        duration = time.time() - self.start_time
+    def result(self, do_print: bool = False) -> "RunningTimer":
+        duration = (time.time() if self.end_time == 0 else self.end_time) - self.start_time
         # Human readable
         hours, remainder = divmod(duration, 3600)
         minutes, seconds = divmod(remainder, 60)
-        print(f"Program executed for {int(hours):02d}:{int(minutes):02d}:{seconds:05.2f} seconds")
+
+        if do_print:
+            print(f"Program executed for {int(hours):02d}:{int(minutes):02d}:{seconds:05.2f} seconds")
+
+        self.elapsed_total_seconds = duration
+        self.elapsed_hours = hours
+        self.elapsed_minutes = minutes
+        self.elapsed_seconds = seconds
+
+        return self
